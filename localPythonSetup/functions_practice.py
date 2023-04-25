1c27c3e66a6711179103cea5549b35573dd3317c
Emily Bacon <111798698+elle-bacon@users.noreply.github.com>
2023-04-24 20:41:23 -0700

    Add files via upload

---
+++ localPythonSetup/functions_practice.py
@@ -0,0 +1,7 @@
+hello() = print("hello ad welcome")
+
+pack() = ["apple", "orange", "banana"]
+
+eat_lunch() = print("first i eat the [0] then i eat the [1] and finally i eat the [2]")
+
+finished_lunch() = print("i ate everything for lunch!")
